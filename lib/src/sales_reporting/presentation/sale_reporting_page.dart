3adecbd4d3f4b75fd0e7978c2ede43bc9368a096
vallejohn <valjohn.teruel@gmail.com>
2024-01-21 17:38:32 +0800

    Update sale_reporting_page.dart

--- lib/src/sales_reporting/presentation/sale_reporting_page.dart
+++ lib/src/sales_reporting/presentation/sale_reporting_page.dart
@@ -12,6 +12,8 @@ class _SaleReportingPageState extends State<SaleReportingPage> {
   final TextEditingController _productNameController = TextEditingController();
   final TextEditingController _quantityController = TextEditingController();
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,137 +38,158 @@ class _SaleReportingPageState extends State<SaleReportingPage> {
             child: BlocBuilder<SalesReportingBloc, SalesReportingState>(builder: (srContext, srState) {
               bool loading = srState.status == SalesReportingStatus.loading;
               bool loadingList = srState.status == SalesReportingStatus.loadingReportsList;
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  TextField(
-                    focusNode: FocusNode(),
-                    enableInteractiveSelection: false,
-                    onTap: () {
-                      AutoRouter.of(context).push(const SearchProductsRoute());
-                    },
-                    controller: _productNameController,
-                    decoration: const InputDecoration(
-                      labelText: 'Local Product',
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextField(
-                    keyboardType: TextInputType.number,
-                    controller: _quantityController,
-                    decoration: const InputDecoration(
-                      labelText: 'Quantity Sold (pcs.)',
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      /*Expanded(
-                        child: FilledButton.tonal(
-                          onPressed: () {
-                            AutoRouter.of(context).push(const AddProductsRoute());
-                          },
-                          child: const Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Icon(Icons.add, size: 17),
-                              SizedBox(width: 15),
-                              Text('Add products'),
-                            ],
-                          ),
-                        ),
-                      ),*/
-                      const SizedBox(
-                        width: 5,
+              return Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    TextFormField(
+                      focusNode: FocusNode(),
+                      enableInteractiveSelection: false,
+                      onTap: () {
+                        AutoRouter.of(context).push(const SearchProductsRoute());
+                      },
+                      controller: _productNameController,
+                      decoration: const InputDecoration(
+                        labelText: 'Local Product',
                       ),
-                      Expanded(
-                        child: FilledButton(
-                          onPressed: loading
-                              ? null
-                              : () {
-                                  final salesReport = SalesReport(
-                                    transactionId: 'TXN0034',
-                                    product: context.read<SearchProductCubit>().state.selectedProduct,
-                                    quantitySold: int.parse(_quantityController.text),
-                                    transactionDate: DateTime.now().toString(),
-                                  );
-                                  srContext.read<SalesReportingBloc>().add(SalesReportingEvent.onSendReport(salesReport));
-                                },
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              if (!loading) const Icon(Icons.send, size: 17),
-                              const SizedBox(width: 15),
-                              Text(loading ? 'Sending report...' : 'Send report'),
-                              if (loading)
-                                SizedBox(
-                                    width: 20,
-                                    height: 20,
-                                    child: CircularProgressIndicator(
-                                      strokeWidth: 2,
-                                      color: Theme.of(context).disabledColor,
-                                    )),
-                            ],
-                          ),
-                        ),
+                      validator: (value){
+                        if(value != null){
+                          if(value.isEmpty) return 'Enter product name';
+                          return null;
+                        }else{
+                          return 'Enter product name';
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.number,
+                      controller: _quantityController,
+                      decoration: const InputDecoration(
+                        labelText: 'Quantity Sold (pcs.)',
                       ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  if (loadingList) const LinearProgressIndicator(),
-                  if (!loadingList)
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width,
-                      child: DataTable(
-                        sortAscending: true,
-                        columns: const <DataColumn>[
-                          DataColumn(
-                            label: Expanded(
-                              child: Text(
-                                'Date',
-                                style: TextStyle(fontStyle: FontStyle.italic),
-                              ),
-                            ),
-                          ),
-                          DataColumn(
-                            label: Expanded(
-                              child: Text(
-                                'Product',
-                                style: TextStyle(fontStyle: FontStyle.italic),
-                              ),
-                            ),
-                          ),
-                          DataColumn(
-                            label: Expanded(
-                              child: Text(
-                                'Quantity',
-                                style: TextStyle(fontStyle: FontStyle.italic),
-                              ),
-                            ),
-                          ),
-                        ],
-                        rows: <DataRow>[
-                          for (int i = 0; i < srState.salesReports.length; i++)
-                            DataRow(
-                              cells: <DataCell>[
-                                DataCell(
-                                  Text(DateFormat.yMd().format(DateTime.parse(srState.salesReports[i].transactionDate))),
-                                ),
-                                DataCell(Text(srState.salesReports[i].product!.name)),
-                                DataCell(Text(srState.salesReports[i].quantitySold.toString())),
+                      validator: (value){
+                        if(value != null){
+                          if(value.isEmpty) return 'Enter quantity';
+                          return null;
+                        }else{
+                          return 'Enter quantity';
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        /*Expanded(
+                          child: FilledButton.tonal(
+                            onPressed: () {
+                              AutoRouter.of(context).push(const AddProductsRoute());
+                            },
+                            child: const Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Icon(Icons.add, size: 17),
+                                SizedBox(width: 15),
+                                Text('Add products'),
                               ],
                             ),
-                        ],
-                      ),
-                    )
-                ],
+                          ),
+                        ),*/
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        Expanded(
+                          child: FilledButton(
+                            onPressed: loading
+                                ? null
+                                : () {
+                                    if(_formKey.currentState!.validate()){
+                                      final salesReport = SalesReport(
+                                        transactionId: 'TXN0034',
+                                        product: context.read<SearchProductCubit>().state.selectedProduct,
+                                        quantitySold: int.parse(_quantityController.text),
+                                        transactionDate: DateTime.now().toString(),
+                                      );
+                                      srContext.read<SalesReportingBloc>().add(SalesReportingEvent.onSendReport(salesReport));
+                                    }
+                                  },
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                if (!loading) const Icon(Icons.send, size: 17),
+                                const SizedBox(width: 15),
+                                Text(loading ? 'Sending report...' : 'Send report'),
+                                if (loading)
+                                  SizedBox(
+                                      width: 20,
+                                      height: 20,
+                                      child: CircularProgressIndicator(
+                                        strokeWidth: 2,
+                                        color: Theme.of(context).disabledColor,
+                                      )),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    if (loadingList) const LinearProgressIndicator(),
+                    if (!loadingList)
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        child: DataTable(
+                          sortAscending: true,
+                          columns: const <DataColumn>[
+                            DataColumn(
+                              label: Expanded(
+                                child: Text(
+                                  'Date',
+                                  style: TextStyle(fontStyle: FontStyle.italic),
+                                ),
+                              ),
+                            ),
+                            DataColumn(
+                              label: Expanded(
+                                child: Text(
+                                  'Product',
+                                  style: TextStyle(fontStyle: FontStyle.italic),
+                                ),
+                              ),
+                            ),
+                            DataColumn(
+                              label: Expanded(
+                                child: Text(
+                                  'Quantity',
+                                  style: TextStyle(fontStyle: FontStyle.italic),
+                                ),
+                              ),
+                            ),
+                          ],
+                          rows: <DataRow>[
+                            for (int i = 0; i < srState.salesReports.length; i++)
+                              DataRow(
+                                cells: <DataCell>[
+                                  DataCell(
+                                    Text(DateFormat.yMd().format(DateTime.parse(srState.salesReports[i].transactionDate))),
+                                  ),
+                                  DataCell(Text(srState.salesReports[i].product!.name)),
+                                  DataCell(Text(srState.salesReports[i].quantitySold.toString())),
+                                ],
+                              ),
+                          ],
+                        ),
+                      )
+                  ],
+                ),
               );
             }),
           ),
